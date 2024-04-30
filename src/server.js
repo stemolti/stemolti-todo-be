8cdad4bd4ca93649150dc7911a61ab8109af9f33
Stefano Agbodan <mystefano95@gmail.com>
2024-04-30 17:28:38 +0200

    Setup MongoDB Server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import app from './app';
+import mongoose from 'mongoose';
+
+mongoose.set('debug', true);
+mongoose.connect('mongodb://127.0.0.1:27017/todos')
+  .then(_ => {
+    const port = 3000;
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
